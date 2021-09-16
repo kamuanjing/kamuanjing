f2b360a6fa41f664768b2588e28fde8d783db218
zxbxns <rokhimarpans@gmail.com>
2021-09-17 02:10:04 +0700

    master

---
+++ del-ip.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/home/listip.txt")
+	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
+		clear
+		echo ""
+		echo "You have no existing clients!"
+		exit 1
+	fi
+
+	clear
+	echo ""
+	echo -e "  Select the existing client you want to remove"
+	echo -e "  Press CTRL+C to return"
+	echo -e "  ====================================="
+	echo -e "     No |  User  |      Ip     |  Exp"
+        echo -e "  ====================================="
+        grep -E "^### " "/home/listip.txt" | cut -d ' ' -f 2-7 | nl -s ') '
+	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
+		if [[ ${CLIENT_NUMBER} == '1' ]]; then
+			read -rp "Select one client [1]: " CLIENT_NUMBER
+		else
+			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
+		fi
+	done
+user=$(grep -E "^### " "/home/listip.txt" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+ip=$(grep -E "^### " "/home/listip.txt" | cut -d ' ' -f 5 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^### " "/home/listip.txt" | cut -d ' ' -f 7 | sed -n "${CLIENT_NUMBER}"p)
+sed -i "/^### | $user | $ip | $exp/d" /home/listip.txt
+sed -i "/^$ip/d" /home/vps/public_html/akses/token
+clear
+echo " Your IP Has Been Deleted"
+echo " =========================="
+echo " Ip         : $ip"
+echo " Name       : $user"
+echo " Expired On : $exp"
+echo " =========================="
---
+++ up.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+clear 
+echo -e "-----------------------------------"
+echo -e " SCRIPT AUTO UPLOAD FILE TO GITHUB "
+echo -e "       Telegram t.me/zxbxns        "
+echo -e "-----------------------------------"
+echo -e "[1] setup global user.name dan email"
+echo -e "[2] upload"
+read -p "Masukan Nomor: " gass
+case $gass in
+1)
+read -p "Masukan Email Akun Github Nya: " email
+git config --global user.email "$email"
+read -p "Masukan Username Github Nya: " user
+git config --global user.name "$user"
+;;
+2)
+git init 
+read -p "Masukan Nama File: " file
+git add $file
+read -p "Masukan Nama Branch: " branch
+git commit -m "$branch"
+sleep 5
+clear
+read -p "Masukan Link Github Mu: " link
+git remote add origin $link
+git push -u origin $branch
+;;
+esac
