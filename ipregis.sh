21c3d9eb8dd1d38e2a5e2edc0c51332a3e54f921
kamuanjing <rokhimarpans@gmail.com>
2021-10-09 22:34:40 +0700

    p

--- ipregis.sh
+++ ipregis.sh
@@ -1,8 +1,8 @@
 #!bin/bash
 cd /usr/bin
-wget -O add-ip "script.zxbxns.me/sc/add-ip.sh"
-wget -O del-ip "script.zxbxns.me/sc/del-ip.sh"
-wget -O xp-ip "script.zxbxns.me/sc/xp-ip.sh"
+wget -O add-ip "https://raw.githubusercontent.com/kamuanjing/kamuanjing/p/add-ip.sh"
+wget -O del-ip "https://raw.githubusercontent.com/kamuanjing/kamuanjing/p/del-ip.sh"
+wget -O xp-ip "https://raw.githubusercontent.com/kamuanjing/kamuanjing/p/xp-ip.sh"
 chmod +x add-ip
 chmod +x del-ip
 chmod +x xp-ip
