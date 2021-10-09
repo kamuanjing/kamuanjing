04ae9d743dfb974416e1de2902617f0dd12cfc8a
kamuanjing <rokhimarpans@gmail.com>
2021-10-09 23:12:52 +0700

    p

--- up.sh
+++ up.sh
@@ -4,15 +4,17 @@ echo -e "-----------------------------------"
 echo -e " SCRIPT AUTO UPLOAD FILE TO GITHUB "
 echo -e "       Telegram t.me/zxbxns        "
 echo -e "-----------------------------------"
-echo -e "[1] setup global user.name dan email"
+echo -e "[1] Setup Config"
 echo -e "[2] upload"
 read -p "Masukan Nomor: " gass
 case $gass in
 1)
-read -p "Masukan Email Akun Github Nya: " email
+read -p "Masukan Email Github: " email
 git config --global user.email "$email"
-read -p "Masukan Username Github Nya: " user
+read -p "Masukan Username Github: " user
 git config --global user.name "$user"
+read -p "Masukan Link Repository: " repo
+echo "$repo" >> /home/repo
 ;;
 2)
 git init 
@@ -20,9 +22,10 @@ read -p "Masukan Nama File: " file
 git add $file
 read -p "Masukan Nama Branch: " branch
 git commit -m "$branch"
-sleep 5
+sleep 3
 clear
-read -p "Masukan Link Github Mu: " link
+link=$(cat /home/repo)
+clear
 git remote add origin $link
 git push -u origin $branch
 ;;
