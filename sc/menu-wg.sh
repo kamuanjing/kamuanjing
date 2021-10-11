bd455af17593f70426a5abcb86b416459187e4e3
kamuanjing <rokhimarpans@gmail.com>
2021-10-12 01:00:53 +0700

    auti

--- sc/menu-wg.sh
+++ sc/menu-wg.sh
@@ -28,7 +28,7 @@ echo -e "    [xx] Back To Menu $red[menu]$NC"
 echo -e ""
 echo -e " ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 echo -e ""
-read -p "    [••] Please Input Number  [1-5 or x] : "  wgr
+read -p "    [••] Please Input Number  [1-5 or xx] : "  wgr
 echo -e ""
 case $wgr in
 1)
